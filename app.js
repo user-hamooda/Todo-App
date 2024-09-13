6436559a68c681d129d71cf3f359248450197937
Mohammed Alamoudi <bl3ck.2005@gmail.com>
2024-09-13 16:59:10 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const todoListUL = document.getElementById('todo-list');
 let allTodos = getTodos();
 updateTodoList();
 
-todoForm.addEventListener('submit', function(e){
+todoForm.addEventListener('submit', function (e) {
     e.preventDefault();
     addTodo();
 })
 
-function addTodo(){
+function addTodo() {
     const todoText = todoInput.value.trim();
-    if(todoText.length > 0){
+    if (todoText.length > 0) {
         const todoObject = {
             text: todoText,
             completed: false
@@ -24,16 +24,16 @@ function addTodo(){
     }
 }
 
-function updateTodoList(){
+function updateTodoList() {
     todoListUL.innerHTML = "";
-    allTodos.forEach((todo, todoIndex)=> {
+    allTodos.forEach((todo, todoIndex) => {
         TodoItem = createTodoItem(todo, todoIndex);
         todoListUL.append(TodoItem);
     })
 }
 
-function createTodoItem(todo, todoIndex){
-    const todoId = "todo-"+todoIndex;
+function createTodoItem(todo, todoIndex) {
+    const todoId = "todo-" + todoIndex;
     const todoLI = document.createElement("li");
     const todoText = todo.text;
     todoLI.className = "todo";
@@ -46,14 +46,17 @@ function createTodoItem(todo, todoIndex){
     </label>
 
     <button class="delete-button">
-    </button> 
+        <svg fill="var(--secondary)" xmlns="http://www.w3.org/2000/svg" width="30" height="30" class="bi bi-x" viewBox="0 0 16 16">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+            </svg>
+    </button>
     `
     const deleteButton = todoLI.querySelector(".delete-button");
-    deleteButton.addEventListener("click", ()=>{
+    deleteButton.addEventListener("click", () => {
         deleteTodoItem(todoIndex);
     })
     const checkbox = todoLI.querySelector("input");
-    checkbox.addEventListener("change", ()=>{
+    checkbox.addEventListener("change", () => {
         allTodos[todoIndex].completed = checkbox.checked;
         saveTodos();
     })
@@ -61,18 +64,18 @@ function createTodoItem(todo, todoIndex){
     return todoLI;
 }
 
-function deleteTodoItem(todoIndex){
-    allTodos = allTodos.filter((_, i)=> i !== todoIndex);
+function deleteTodoItem(todoIndex) {
+    allTodos = allTodos.filter((_, i) => i !== todoIndex);
     saveTodos();
     updateTodoList();
 }
 
-function saveTodos(){
+function saveTodos() {
     const todoJson = JSON.stringify(allTodos);
     localStorage.setItem("todos", todoJson);
 }
 
-function getTodos(){
+function getTodos() {
     const todos = localStorage.getItem("todos") || "[]";
     return JSON.parse(todos);
 }
